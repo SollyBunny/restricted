46c19001ff7fd98a3660c060903c43e74de71e5c
SollyBunny <sollybunn@gmail.com>
2023-12-18 07:57:25 +0000

    small balancing

--- stubserverhost.js
+++ stubserverhost.js
@@ -1,14 +1,18 @@
-const ws = new WebSocket(`wss://${document.location.host}${document.location.pathname}stubserverserver?host`);
 
-ws.onmessage = msg => {
-	const data = msg.data;
-	const action = data.slice(0, 1);
-	const key = data.slice(1)
-	if ("tfgh".indexOf(key) === -1) return;
-	if (action === "u") {
-		keys[key] = false;
-	} else {
-		keys[key] = true;
-	}
-	updateControls();
-};
+function stubserverhost() {
+	let ws = new WebSocket(`wss://${document.location.host}${document.location.pathname}stubserverserver?host`);
+
+	ws.onmessage = msg => {
+		const data = msg.data;
+		const action = data.slice(0, 1);
+		const key = data.slice(1)
+		if ("tfgh".indexOf(key) === -1) return;
+		if (action === "u") {
+			keys[key] = false;
+		} else {
+			keys[key] = true;
+		}
+		updateControls();
+	};
+}
+console.log("Run stubserverhost() to start hosting")
